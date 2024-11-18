4ca5d5c85ee5cbaab15ef53341db5c36a597538b
Mustapha bousil <bousilmustapha@gmail.com>
2024-11-18 18:48:54 +0100

    integrating docker compose

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const path = require('path')
 const app = express()
 const port=3000
 
+const replicas = process.env.APP_NAME
+
 app.use('/images', express.static(path.join(__dirname, 'images')))
 app.use('/',(req,res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
-    console.log('Request served by node app')
+    console.log(`Request served by ${replicas}`)
 })
 
 app.listen(port, () => {
